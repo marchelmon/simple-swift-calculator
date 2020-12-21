a5a7d3d1165b746a8277be0e77bbe193a076cbef
Marc Hulterström <marc.hulterstrom@gmail.com>
2020-12-21 13:30:54 +0100

    Starting Xcode project

---
+++ Simple Calculator/Simple Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Calculator
+//
+//  Created by marchelmon on 2020-12-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
