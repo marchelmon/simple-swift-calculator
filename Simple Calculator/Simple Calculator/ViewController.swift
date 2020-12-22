10724afa87c2f9788a6a5ac81d10ff0c92a19ba8
Marc Hulterström <marc.hulterstrom@gmail.com>
2020-12-22 11:28:01 +0100

    Adding button actions and label outlet to viewcontroller

--- Simple Calculator/Simple Calculator/ViewController.swift
+++ Simple Calculator/Simple Calculator/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
